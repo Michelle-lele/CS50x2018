87ad434bf654ca53b2ada27420ade9506d9132ae
Michelle-lele <Michelle-lele@users.noreply.github.com>
2018-06-03 10:35:44 +0000

    20180603T103528Z

--- mario.py
+++ mario.py
@@ -1,16 +1,16 @@
 from cs50 import get_int
 
-#if height's not positive number or less than 24, promp user
+# if height's not positive number or less than 24, promp user
 while True:
-    #get user input for pyramid height
-    print("Height: ", end = '')
+    # get user input for pyramid height
+    print("Height: ", end='')
     height = get_int()
     if height >= 0 and height <= 23:
         break
 row = 1
 for row in range(height):
-    #print as much spaces as needed depending on specified height
-    print(" " * (height - row - 1), end = '')
-    print("#" * (row + 1), end = '')
-    print("  ", end = '')
+    # print as much spaces as needed depending on specified height
+    print(" " * (height - row - 1), end='')
+    print("#" * (row + 1), end='')
+    print("  ", end='')
     print("#" * (row + 1))
